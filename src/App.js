8419d3afed4174de86c3c5247dd5dea0a9e01698
Clifford McCall <cliff@Cliffords-MacBook-Pro.local>
2020-07-26 19:03:23 -0400

    Added filter and validation on submit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,86 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import PeopleList from './components/PeopleList';
-import { Button, Form, Card } from 'semantic-ui-react';
-import Background from './assets/images/star-wars-logo.jpg';
-
+import React, { Component } from "react";
+import Axios from "axios";
+import PeopleList from "./components/PeopleList";
+import { Button, Form, Card } from "semantic-ui-react";
+import Background from "./assets/images/star-wars-logo.jpg";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        people: [],
-        count: 0
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.clearSearch = this.clearSearch.bind(this);
-
-    }
+    this.state = {
+      people: [],
+      count: 0,
+      filterCount: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearSearch = this.clearSearch.bind(this);
+  }
 
   async getPeople() {
-    return Axios.get("http://swapi.dev/api/people")
-      .then((response) => {
-        this.setState ({ people: response.data.results})
-      })
+    return Axios.get("http://swapi.dev/api/people").then((response) => {
+      this.setState({ people: response.data.results });
+    });
+  }
+
+  componentDidMount() {
+    this.getPeople();
   }
 
   handleChange(event) {
-    this.setState({count: event.target.value});
+    this.setState({ count: event.target.value });
   }
 
   clearSearch(event) {
     event.preventDefault();
-    this.setState({count: 0});
+    this.setState({ count: 0, filterCount: 0 });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.getPeople();
+    this.setState({ filterCount: this.state.count });
   }
 
   render() {
-    const {people} = this.state;
+    const { people } = this.state;
     return (
-      <div style={{ 
-        backgroundImage: `url(${Background})`,
-        backgroundSize: 'contain',
-        height: '100vh' }} 
+      <div
         className="App"
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundSize: "contain",
+          height: "100vh",
+        }}
       >
-        <Card style={{ margin: 'auto' }}>
-        <Form style={{ maxWidth: 275, padding: 10, margin: 'auto'}} onSubmit={this.handleSubmit}>
-          <Form.Field>
-          <h1>Star Wars API</h1>
-            <label>Enter A Number Between 1 - 10</label>
-            <input 
-              type="text" 
-              value={this.state.count} 
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button color='yellow' type="submit" value="Submit">Submit</Button>
-          <Button color='black' onClick={this.clearSearch}>Clear Search</Button>
-        </Form>
+        <Card style={{ margin: "auto" }}>
+          <Form
+            style={{ maxWidth: 275, padding: 10, margin: "auto" }}
+            onSubmit={this.handleSubmit}
+          >
+            <h1>Star Wars API</h1>
+            <Form.Field>
+              <label>Enter A Number Between 1 - 10</label>
+              <input
+                type="text"
+                value={this.state.count}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
+            <Button
+              color="yellow"
+              type="submit"
+              value="Submit"
+              disabled={this.state.count > 10 || this.state.count < 0}
+            >
+              Submit
+            </Button>
+            <Button color="black" onClick={this.clearSearch}>
+              Clear Search
+            </Button>
+          </Form>
         </Card>
-        <PeopleList people={people} count={this.state.count}/>
+        <PeopleList people={people} count={this.state.filterCount} />
       </div>
-    )
+    );
   }
 }
--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { Card, List } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Card, List } from "semantic-ui-react";
 
 export default class PeopleList extends Component {
-    render() {
-        const people = this.props.people.filter((count, index) => index < this.props.count);
-        return (
-            <div>
-                {
-                    people.map((p) => {
-                        return (
-                            <Card 
-                                style={{width:275, display: "inline-block", padding: '10px'}} 
-                                variant="outlined"  
-                                key={p.url}
-                            >
-                                <Card.Content>
-                                    <Card.Header>{p.name}</Card.Header>
-                                    <Card.Meta>Birth Year: {p.birth_year}</Card.Meta>
-                                    <Card.Description>
-                                        <List.Item>Gender: {p.gender}</List.Item>
-                                        <List.Item>Mass: {p.mass}</List.Item>
-                                        <List.Item>Height: {p.height}</List.Item>
-                                        <List.Item>Hair Color: {p.hair_color}</List.Item>
-                                    </Card.Description>
-                                </Card.Content>
-                            </Card>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+  render() {
+    const people = this.props.people.filter(
+      (count, index) => index < this.props.count
+    );
+    return (
+      <div style={{ width: "90%", margin: "auto" }}>
+        {people.map((p) => {
+          return (
+            <Card
+              style={{ width: 275, display: "inline-block", padding: "10px" }}
+              variant="outlined"
+              key={p.url}
+            >
+              <Card.Content>
+                <Card.Header>{p.name}</Card.Header>
+                <Card.Meta>Birth Year: {p.birth_year}</Card.Meta>
+                <Card.Description>
+                  <List.Item>Gender: {p.gender}</List.Item>
+                  <List.Item>Mass: {p.mass}</List.Item>
+                  <List.Item>Height: {p.height}</List.Item>
+                  <List.Item>Hair Color: {p.hair_color}</List.Item>
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
 }
